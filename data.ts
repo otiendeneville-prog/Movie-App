6308bccce2c24677dd8d81d9ee713e70e6e28635
Neuville <otiendeneville@gmail.com>
2026-07-17 08:49:17 +0300

    adding game of thrones array

--- data.ts
+++ data.ts
@@ -1,4 +1,5 @@
 export const movies = [
   { id: "1", title: "The Matrix", rating: 8.7 },
   { id: "2", title: "Insterstellar", rating: 8.6 },
-];
+  {id:"3", title:"Parasite", rating:8.5},
+  {id:"4", title:"Game Of Thrones", rating:9.2 },
