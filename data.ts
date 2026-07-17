2fb214db6c6cfe1e630122189f86cbb382523bd2
Neuville <otiendeneville@gmail.com>
2026-07-17 08:51:05 +0300

    adding gotfather array

--- data.ts
+++ data.ts
@@ -3,3 +3,5 @@ export const movies = [
   { id: "2", title: "Insterstellar", rating: 8.6 },
   {id:"3", title:"Parasite", rating:8.5},
   {id:"4", title:"Game Of Thrones", rating:9.2 },
+  {id:"5", title:"God Father", rating:8.8 },
+  
