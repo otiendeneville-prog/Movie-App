505f421db77f68371353af492b1e645259636f15
Neuville <otiendeneville@gmail.com>
2026-07-20 14:14:48 +0300

    adding new version of expo

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View className="text-center flex-1 justify-center">
+      <Text className="color-accent font-bold p-5xl">Home</Text>
     </View>
   );
 };
