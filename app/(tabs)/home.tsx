bbd98d3ae028228a5c996153b010b65848776d9e
Neuville <otiendeneville@gmail.com>
2026-07-20 22:53:34 +0300

    adding and importing safe area view

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import { StatusBar, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
   return (
@@ -6,6 +7,9 @@ const Home = () => {
       <Text className="color-accent text-[50px] text-center justify-center font-bold p-20xl">
         Welcome To My App
       </Text>
+      <SafeAreaView className="-mb-2">
+        <StatusBar style="light" />
+      </SafeAreaView>
     </View>
   );
 };
