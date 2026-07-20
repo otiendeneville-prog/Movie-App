e604f0c58f5c6bfeefef6df4a15b5c016ea10dff
Neuville <otiendeneville@gmail.com>
2026-07-20 15:41:37 +0300

    changing the text font size

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -3,8 +3,8 @@ import { Text, View } from "react-native";
 const Home = () => {
   return (
     <View className="text-center flex-1 justify-center">
-      <Text className="color-accent text-center justify-center font-bold p-20xl">
-        Home
+      <Text className="color-accent text-[50px] text-center justify-center font-bold p-20xl">
+        Welcome To My App
       </Text>
     </View>
   );
