ed5b765cc61e4fd38bb66ba5805937064a7be941
Neuville <otiendeneville@gmail.com>
2026-07-20 22:49:24 +0300

    adding the backgroundcolor of home page to neutral

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 const Home = () => {
   return (
-    <View className="text-center flex-1 justify-center">
+    <View className="text-center flex-1 justify-center bg-neutral-800">
       <Text className="color-accent text-[50px] text-center justify-center font-bold p-20xl">
         Welcome To My App
       </Text>
