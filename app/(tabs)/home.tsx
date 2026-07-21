4ee0fbe36324c3eb849795216c8643de8b29eafc
Neuville <otiendeneville@gmail.com>
2026-07-21 07:58:09 +0300

    adding touchable opacity

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,15 +1,16 @@
-import { StatusBar, Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
   return (
     <View className="text-center flex-1 justify-center bg-neutral-800">
-      <Text className="color-accent text-[50px] text-center justify-center font-bold p-20xl">
-        Welcome To My App
-      </Text>
-      <SafeAreaView className="-mb-2">
-        <StatusBar style="light" />
-      </SafeAreaView>
+      <TouchableOpacity>
+        <Text className="color-accent text-[50px] text-center justify-center font-bold p-20xl">
+          Welcome To My App
+        </Text>
+      </TouchableOpacity>
+
+      <SafeAreaView className="-mb-2"></SafeAreaView>
     </View>
   );
 };
