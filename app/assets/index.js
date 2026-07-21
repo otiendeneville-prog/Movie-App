3598b3d391074d14657c99c9705d914319620842
Neuville <otiendeneville@gmail.com>
2026-07-21 09:53:40 +0300

    import imageBackground

---
+++ app/assets/index.js
@@ -0,0 +1,10 @@
+const images = {
+  icon: require("./icon.png"),
+  favicon: require("./favicon.png"),
+  splashIcon: require("./splash-icon.png"),
+  bg: require("./bg.png"),
+  highlight: require("./highlight.png"),
+  logo: require("./logo.png"),
+};
+
+export { images };
