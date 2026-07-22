3e874465d741b1b8647dfd4baaaed6f2c4220db6
Neuville <otiendeneville@gmail.com>
2026-07-22 20:11:54 +0300

    changing the home text to accent

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -9,7 +9,7 @@ const Home = () => {
       className="text-center flex-1 items-center justify-center "
     >
       <TouchableOpacity>
-        <Text className="color-secondary text-[50px] text-center justify-center font-bold p-20xl">
+        <Text className="color-accent text-[50px] text-center justify-center font-bold p-20xl">
           Welcome To My App
         </Text>
       </TouchableOpacity>
