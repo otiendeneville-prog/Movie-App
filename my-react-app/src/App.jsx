f346593226ee481751601b4e554602d99ede2c4b
Neuville <otiendeneville@gmail.com>
2026-07-23 15:03:27 +0300

    delete the app errror

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,17 +1,19 @@
-const Card = ({title}) => app {
+const Card = ({title}) => { 
 return(
   <div>
   <h1>{title}</h1>
   </div>
 )
-};
+}
 
-const App = () => {
+const App = ({title}) => {
   return(
     <div>
       <h1>Function Arrow Component</h1>
       <Card  title = "Avartar"/>
       <Card title= "Spider Man" />
+      <Card title = "Avangers" />
+
     </div>
   )
 }
