dffb3865e8f13675366bd44819ad7268f48276aa
Neuville <otiendeneville@gmail.com>
2026-07-23 16:32:01 +0300

    use state for count and useEffect

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { use, useEffect, useState } from 'react'
 
 const Card = ({title}) => {
+  const [hasLiked, setHasLiked] = useState(false);
+  const [count, setCount ] = useState(0)
+
+  useEffect( () =>{
+    console.log(`{$title has been liked :{hasliked}}`);
+  },deps[hasLiked]);
+  
+  useEffect( ()=>{
+    console.log(`CARD RENDERED`)
+  },deps[]);
   return (
-    <h2>{title}</h2>
+    <div className='card'>
+      
+      <h2>{title}</h2>
+      <button onClick={ ()=> setHasLiked(!hasLiked)}>{hasLiked ? "❤️" : "🤍"}</button>
+      
+      
+    </div>
   )
 }
-
 const App = () => {
   return(
-    <div>
-      <h2>Functional Arrow Components</h2>
+    <div className='card-container'>
       <Card title ="Avatar"/>
       <Card  title ="Spider Man"/>
       <Card title ="Avangers" />
