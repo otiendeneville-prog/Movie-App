dd431b7950d13f7f4e84714665ff2a80cfb364af
Neuville <otiendeneville@gmail.com>
2026-07-23 17:17:14 +0300

    add tailwind smart complition snippet

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function App() {
   return (
-    <div className='text-3xl font-bold underline'>Hello World</div>
+    <div className='text-3xl font-bold underline text-red-500'>Hello World</div>
   )
 }
 
