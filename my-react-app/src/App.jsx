ca7418499dddc13992a08cadd0b0330914945659
Neuville <otiendeneville@gmail.com>
2026-07-23 14:59:28 +0300

    add the avartar and the spider man cards

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,10 +4,14 @@ return(
   <h1>{title}</h1>
   </div>
 )
-}
+};
 
 const App = () => {
   return(
-    <h1>Function Arrow Component</h1>
+    <div>
+      <h1>Function Arrow Component</h1>
+      <Card  title = "Avartar"/>
+      <Card title= "Spider Man" />
+    </div>
   )
 }
